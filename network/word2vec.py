87aca1e81318338aee13d9bd529aeffaa8e49fee
陈冠仲 <abigballoon@foxmail.com>
2018-08-31 09:09:42 +0800

    add batch operation

--- network/word2vec.py
+++ network/word2vec.py
@@ -3,9 +3,12 @@ from sklearn.manifold import TSNE
 import matplotlib.pyplot as plt
 import pickle
 import random
+import time
+import json
 
 torch.set_default_dtype(torch.float64)
 
+# with open('shakespeare.dev', 'rb') as f:
 with open('shakespeare.dev', 'rb') as f:
     corpus = pickle.load(f)
 
@@ -14,6 +17,7 @@ for word in corpus:
     if word not in vocabd:
         vocabd[word] = 0
     vocabd[word] += 1
+print(len(corpus))
 vocab = list(vocabd.items())
 vocab.sort(key=lambda x: x[1], reverse=True)
 
@@ -24,74 +28,117 @@ class Word2Vec(object):
     def __init__(self, vocab, embed_dim, eta=None):
         self.eta = eta or 1.0
         self.vocab = vocab
+        self.dict = {word: idx for idx, word in enumerate(vocab)}
         self.vocab_dim = len(self.vocab)
         self.embed_dim = embed_dim
         self.ibed = torch.randn(self.embed_dim, self.vocab_dim, requires_grad=True)
         self.obed = torch.randn(self.embed_dim, self.vocab_dim, requires_grad=True)
 
-    def find(self, word):
-        try:
-            idx = self.vocab.index(word)
-        except ValueError:
-            idx = len(self.vocab) - 1
-        return idx
+    def find(self, words):
+        ids = []
+        for word in words:
+            idx = self.dict.get(word, None)
+            if idx == None:
+                idx = self.dict.get('<UNK>')
+            ids.append(idx)
+        return ids
 
-    def select(self, idx, word_type='i'):
-        _filter = torch.zeros(self.vocab_dim, 1)
-        _filter[idx][0] = 1
-        if word_type == 'i':
-            return self.ibed.mm(_filter)
-        else:
-            return self.ibed.mm(_filter)
+    def select(self, ids, word_type='i'):
+        return self.ibed.t()[ids]
 
-    def lookup(self, word, word_type):
-        idx = self.find(word)
-        return self.select(idx, word_type)
 
-    def _forward(self, corpus, window=2, neg_size=5):
+    def lookup(self, words):
+        ids = self.find(words)
+        return self.select(ids)
+
+    def _forward(self, batch_size, corpus, window=2, neg_size=5):
+        starttime = time.time()
+        L = len(corpus)
+        for start in range(0, L, batch_size):
+            end = start + batch_size
+            if end > L:
+                end = L
+            targets = []
+            contexts1 = []
+            contexts2 = []
+            for idx in range(start, end):
+                if not idx % 1000:
+                    current = time.time()
+                    print("%f%%"%(idx / len(corpus) * 100))
+                    print("estimated:", (current - starttime) * (len(corpus) - idx) / 1000.0 / 60.0 / 60.0)
+                    starttime = time.time()
+
+                targets_start = idx - window
+                if targets_start < 0:
+                    targets_start = 0
+                _targets = corpus[targets_start: idx] + corpus[idx + 1: idx + 1 + window]
+                targets += _targets
+                contexts1 += [idx for _ in _targets]
+                contexts2 += [idx for _ in range(neg_size * len(_targets))]
+            target_embededs = self.lookup(targets)
+            context_embededs = self.lookup(contexts1)
+            loss = torch.log(torch.sigmoid(target_embededs * context_embededs)).sum()
+            neg_embededs = self.negative(len(contexts2))
+            context_embededs = self.lookup(contexts2)
+            loss += torch.log(torch.sigmoid(-neg_embededs * context_embededs)).sum()
+
+            if not torch.isnan(loss.sum()) and not torch.isinf(loss.sum()):
+                loss_sum = -loss.sum()
+                self.backward(loss_sum.sum(), len(targets))
+
+        """
         for idx, word in enumerate(corpus):
             if not idx % 1000:
+                current = time.time()
                 print("%f%%"%(idx / len(corpus) * 100))
+                print("estimated:", (current - start) * (len(corpus) - idx) / 1000.0 / 60.0 / 60.0)
+                start = time.time()
+            if random.random() > 0.3:
+                continue
             targets_start = idx - window
             if targets_start < 0:
                 targets_start = 0
             targets = corpus[targets_start: idx] + corpus[idx + 1: idx + 1 + window]
-            context_embeded = self.lookup(word, 'i')
-            context_idx = self.find(word)
+            context_embededs = self.lookup([idx for _ in targets], 'i')
             loss_sum = torch.zeros(1, )
             switch = False
 
-            for target in targets:
-                target_embeded = self.lookup(target, 'o')
-                loss = torch.log(torch.sigmoid(target_embeded.t().mm(context_embeded)))
-                for _ in range(neg_size):
-                    neg_embeded = self.negative(context_idx)
-                    loss += torch.log(torch.sigmoid(-neg_embeded.t().mm(context_embeded)))
-                if not torch.isnan(loss.sum()) and not torch.isinf(loss.sum()):
-                    loss_sum -= loss.sum()
-                    switch = True
+            target_embededs = self.lookup(targets, 'o')
+            loss = torch.log(torch.sigmoid(target_embededs * context_embededs)).sum()
+
+            neg_embededs = self.negative(neg_size)
+            context_embededs = self.lookup([idx for _ in range(neg_size)], 'i')
+            loss += torch.log(torch.sigmoid(-neg_embededs * context_embededs)).sum()
+            if not torch.isnan(loss.sum()) and not torch.isinf(loss.sum()):
+                loss_sum -= loss.sum()
+                switch = True
             if switch:
                 self.backward(loss_sum.sum(), len(targets))
+        """
 
-    def forward(self, corpus, epochs, window=2, neg_size=5):
+    def forward(self, batch_size, corpus, epochs, window=2, neg_size=5):
         for _ in range(epochs):
-            self._forward(corpus, window=window, neg_size=neg_size)
+            self._forward(batch_size, corpus, window=window, neg_size=neg_size)
 
     def backward(self, loss, batch_size):
         loss.backward()
-        print(loss.sum())
         with torch.no_grad():
             self.ibed -= self.eta * self.ibed.grad / batch_size
             self.ibed.grad.zero_()
             # self.obed -= self.eta * self.obed.grad / batch_size
             # self.obed.grad.zero_()
 
-    def negative(self, idx):
-        neg_idx = idx
-        while neg_idx == idx:
-            neg_idx = random.randint(0, self.vocab_dim - 1)
-        return self.select(neg_idx, 'o')
+    def negative(self, count=5):
+        return torch.randn(count, self.embed_dim)
 
+
+    def store(self):
+        data = self.ibed.detach()
+        torch.save(data, 'model.torch')
+
+    def load(self):
+        data = torch.load('model.torch')
+        self.ibed = data
 
     def plot(self, plot_size=500):
         def plot_with_labels(low_dim_embs, labels, filename):
@@ -112,13 +159,14 @@ class Word2Vec(object):
             plt.savefig(filename)
 
         labels = [self.vocab[i] for i in range(plot_size)]
-        tsne = TSNE(perplexity=30, n_components=2, init='pca', n_iter=5000, method="exact")
+        tsne = TSNE(perplexity=30, n_components=2, init='pca', n_iter=7000, method="exact")
         low_dim_embs = tsne.fit_transform(self.ibed.detach().t()[:plot_size, :])
         plot_with_labels(low_dim_embs, labels, "tsne.png")
 
 model = Word2Vec(vocab, 300, eta=0.5)
 
-model.forward(corpus, 1)
+# model.forward(20, corpus, 3, neg_size=5)
 print("training done")
-
-model.plot()
+# model.store()
+model.load()
+model.plot(700)

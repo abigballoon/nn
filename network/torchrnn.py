57bfc6a5271487973117600d78cdc29485fa2707
陈冠仲-chenguanzhong <chenguanzhong@game.ntes>
2018-11-07 20:35:20 +0800

    fix predict bug

--- network/torchrnn.py
+++ network/torchrnn.py
@@ -11,9 +11,11 @@ SOF = "<S>"
 UNK = "<UNK>"
 EOF = "<E>"
 # vocab = list('abcdefghijklmnopqrstuvwxyz') + [EOF, UNK, ]
-vocab = [SOF, ] + list(string.ascii_letters) + [EOF, UNK, ]
+vocab = [SOF, ] + list(string.ascii_letters) + list(" .,;'-") + [EOF, UNK, ]
+print(vocab)
 rvocab = {item: idx for idx, item in enumerate(vocab)}
 vocab_size = len(vocab)
+h_size = 256
 print(vocab_size)
 
 class RNN(nn.Module):
@@ -33,7 +35,7 @@ class RNN(nn.Module):
         self.i2h = nn.Linear(input_size + hidden_size, hidden_size)
         self.o2o = nn.Linear(output_size + hidden_size, output_size)
 
-        self.dropout = nn.Dropout(0.1)
+        self.dropout = nn.Dropout(0.2)
         self.softmax = nn.LogSoftmax(dim=1)
         self.loss = nn.NLLLoss()
         self.learning_rate = learning_rate
@@ -49,7 +51,7 @@ class RNN(nn.Module):
         in_combined = torch.cat([i, h])
         o = self.i2o(in_combined.t())
         next_h = self.i2h(in_combined.t())
-        out_combined = torch.cat([o, next_h], dim=1)
+        out_combined = torch.cat([next_h, o], dim=1)
         output = self.o2o(out_combined)
         drop = self.dropout(output)
         final_output = self.softmax(drop)
@@ -70,16 +72,17 @@ class RNN(nn.Module):
         while predict_c != EOF and len(result) < 20:
             if loop < len(pre):
                 i = X[loop]
-            o, _ = self.forward(i.unsqueeze(0).t(), h.t())
+            o, h = self.forward(i.unsqueeze(0).t(), h.t())
             c_i = torch.argmax(o)
             predict_c = vocab[c_i.item()]
             result = result + [predict_c, ]
-            i = o.t().squeeze(0)
+            _X, _ = vecs([predict_c, ], rvocab, SOF, EOF, UNK)
+            i = _X[0]
             loop += 1
         return ''.join(result)
 
 def vecs(word, rvocab, sof, eof, unk):
-    word = [sof, ] + list(word) + [eof, ]
+    word = [SOF] + list(word) + [eof, ]
     def find(character):
         return rvocab.get(character, rvocab.get(unk))
 
@@ -89,12 +92,17 @@ def vecs(word, rvocab, sof, eof, unk):
     return word_vec[: -1], torch.LongTensor([find(item) for item in word[1: ]])
 
 def getENData():
-    with codecs.open("en.dev.txt", encoding="utf8") as f:
+    with codecs.open("shakespear.dev.txt", encoding="utf8") as f:
         content = f.read().replace('\n', '')
-    return list(filter(lambda x: len(x) > 1, content.split(' ')))
+    words = list(filter(lambda x: len(x) > 1, content.split(' ')))
+    result = []
+    for item in words:
+        if item not in result:
+            result.append(item)
+    return result
 
 def getNamesData():
-    with codecs.open("/home/cgz/Downloads/names/names/English.txt", encoding="utf8") as f:
+    with codecs.open("./data/names/English.txt", encoding="utf8") as f:
         content = f.read()
     return list(filter(lambda x: len(x) > 1, content.split('\n')))
 
@@ -103,7 +111,7 @@ def train(rnn=None):
     if not rnn:
         rnn = RNN(vocab_size, h_size, vocab_size)
 
-    words = getENData()
+    words = getNamesData()
     random.shuffle(words)
     L = len(words)
     print(L)
@@ -116,35 +124,30 @@ def train(rnn=None):
             x = X[idx]
             y = Y[idx]
 
-            o, h = rnn.forward(x.unsqueeze(0).t(), h.t())
+            o, h = rnn(x.unsqueeze(0).t(), h.t())
             l = rnn.loss(o.t(), y.unsqueeze(0))
+
             loss += l
             sum_loss += loss
+
         loss.backward()
         rnn.backward()
 
-        if not (process + 1) % 5000:
+        if not (process + 1) % 3000:
             print(word)
-            print(sum_loss / 5000)
+            print(sum_loss / 3000)
             print((process + 1) / L)
             sum_loss = 0
     return rnn
     
 
 if __name__ == '__main__':
-    # rnn = torch.load("rnn.torch")
-    rnn = None
-    for _ in range(2):
+    rnn = torch.load("rnn.torch")
+    # rnn = None
+    for _ in range(20):
         rnn = train(rnn)
         torch.save(rnn, "rnn.torch")
-    print(rnn.predict([SOF, "Hel"]))
-    print(rnn.predict([SOF, "He"]))
-    print(rnn.predict([SOF, "H"]))
-    print(rnn.predict([SOF, 'Th']))
-    print(rnn.predict([SOF, 'A']))
-    print(rnn.predict([SOF, 'B']))
-    print(rnn.predict([SOF, 'C']))
-    print(rnn.predict([SOF, 'D']))
-    print(rnn.predict([SOF, 'E']))
-    print(rnn.predict([SOF, 'F']))
+    starts = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'U', 'X']
+    for c in starts:
+        print(rnn.predict([SOF, c]))
 

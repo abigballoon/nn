321ee8b49d70d849cafdc56d3a723551b0d81550
cgz <abigballoon@foxmail.com>
2019-02-03 00:43:15 +0800

    update nce

--- network/nce.py
+++ network/nce.py
@@ -6,6 +6,9 @@ import torch
 import codecs
 import numpy as np
 import random
+import pickle
+import os
+import time
 from matplotlib.font_manager import FontProperties
 
 torch.set_default_dtype(torch.float64)
@@ -22,6 +25,7 @@ class NCE(torch.nn.Module):
         self.bias = torch.nn.Parameter(torch.randn(self.vocab_size, 1).uniform_(-1, 1))
 
         self.vocab = [item[0] for item in vocab]
+        self._vocab = list(reversed(self.vocab))
         self.rvocab = {item: index for index, item in enumerate(self.vocab)}
 
         _sum = sum([item[1] ** 0.75 for item in vocab])
@@ -29,7 +33,7 @@ class NCE(torch.nn.Module):
         self.UNK = unk
 
     def p(self, words):
-        return self.freq[torch.argmax(words, 0)]
+        return self.freq[words]
     
     def select(self, words):
         return self.embed.t()[words]
@@ -41,83 +45,104 @@ class NCE(torch.nn.Module):
         """
         return (V, x), x = batch_size
         """
-        _map = torch.tensor([[idx, self.find(word), ] for idx, word in enumerate(words)])
-        return torch.sparse.FloatTensor(_map.t(), torch.ones(len(words)), torch.Size([len(words), self.vocab_size])).to_dense().t()
+        _map = torch.tensor([self.find(word) for idx, word in enumerate(words)])
+        return _map
 
     def forward(self, targets, contexts, noise_count, re=10):
         """
         target (V, x), one-hots, x = batch_size
         context (V, x), one-hots, x = batch_size
+        target (x, ), indexs, x = batch_size
+        context (x, ), indexs, x = batch_size
         self.embed (E, V)
         self.bias (V, 1)
         """
-        batch_size = targets.size()[1]
+        batch_size = targets.size()[0]
         # target rep
-        q = torch.mm(self.embed, targets)
+        q = self.embed.t()[targets].t()
 
         # context rep
-        r = torch.mm(self.embed, contexts)
+        r = self.embed.t()[contexts].t()
 
         # bias
-        b = torch.mm(self.bias.t(), targets)
+        b = self.bias[targets].t()
 
         # score
         s = ((r * q).sum(0) + b) / self.embed_dim
 
-        # distribution of words
-        # p = self.p(targets + )
-
         loss = (s - torch.log(noise_count * self.p(targets))).sigmoid().log().sum()
 
         noises = self.get_noises(batch_size * noise_count)
+
         noise_loss = (1 - (torch.cat((s, ) * noise_count, dim=1) - torch.log(noise_count * self.p(noises))).sigmoid()).log().sum()
-        
-        return -(loss + noise_loss) / batch_size + re * self.embed.pow(2).sum() / (self.embed_dim * self.vocab_size)
+
+        total_loss = -(loss + noise_loss) / batch_size
+        penalty = re * (q.pow(2).sum() + r.pow(2).sum()) / (self.embed_dim * batch_size)
+        # print(total_loss)
+        # print(penalty)
+        del q
+        del r
+        del b
+        del s
+        del loss
+        del noises
+        del noise_loss
+
+        return total_loss + penalty
 
     def get_targets_n_contexts(self, target_words, context_words):
         return self.indexfy(target_words), self.indexfy(context_words)
 
     def get_noises(self, count):
-        # choice = np.random.choice(self.vocab, count, p=self.freq)
         choice = np.random.choice(self.vocab, count)
-        return self.indexfy(choice)
+        result = self.indexfy(choice)
+        return result
 
-    def store(self):
+    def store(self, name=None):
+        torch.save(self, 'nce.torch')
+
+        with open('vocab.pickle', 'wb+') as f:
+            pickle.dump(self.vocab, f)
+
+    def dump(self, name=None):
         data = self.embed.detach()
-        torch.save(data, 'model.torch')
+        torch.save(data, 'imdb2.torch')
 
     def load(self):
+        return
         data = torch.nn.Parameter(torch.load('model.torch'))
         self.embed = data
         print(self.embed)
 
 
-    def plot(self, plot_size=500):
-        def plot_with_labels(low_dim_embs, labels, filename):
-            font = FontProperties(fname='/usr/share/fonts/truetype/wqy/wqy-microhei.ttc')
-            # plt.rcParams['font.sans-serif'] = ['WenQuanYi Micro Hei', ]
-            plt.rcParams['axes.unicode_minus'] = False
-            plt.figure(figsize=(20, 20))
-            for i, label in enumerate(labels):
-                x, y = low_dim_embs[i, :]
-                plt.scatter(x, y)
-                plt.annotate(
-                    label,
-                    xy=(x, y),
-                    xytext=(5, 2),
-                    textcoords="offset points",
-                    ha="right",
-                    va="bottom",
-                    fontproperties=font,
-                )
-            plt.savefig(filename)
+def plot(self, plot_size=500):
+    def plot_with_labels(low_dim_embs, labels, filename):
+        font = FontProperties(fname='/usr/share/fonts/truetype/wqy/wqy-microhei.ttc')
+        # plt.rcParams['font.sans-serif'] = ['WenQuanYi Micro Hei', ]
+        plt.rcParams['axes.unicode_minus'] = False
+        plt.figure(figsize=(40, 40))
+        for i, label in enumerate(labels):
+            x, y = low_dim_embs[i, :]
+            plt.scatter(x, y)
+            plt.annotate(
+                label,
+                xy=(x, y),
+                xytext=(5, 2),
+                textcoords="offset points",
+                ha="right",
+                va="bottom",
+                fontproperties=font,
+            )
+        plt.savefig(filename)
 
-        print("start plot")
-        labels = [self.vocab[i] for i in range(plot_size)]
-        tsne = TSNE(perplexity=30, n_components=2, init='pca', n_iter=5000, method="exact")
-        low_dim_embs = tsne.fit_transform(self.embed.detach().t()[:plot_size, :])
-        plot_with_labels(low_dim_embs, labels, "tsne_nce.png")
-        print("end plot")
+    print("start plot")
+    labels = [self.vocab[i] for i in range(plot_size)]
+    data = self.embed.detach().t()[:plot_size, :]
+    del self
+    tsne = TSNE(perplexity=30, n_components=2, init='pca', n_iter=5000, method="exact")
+    low_dim_embs = tsne.fit_transform(data)
+    plot_with_labels(low_dim_embs, labels, "tsne_nce.png")
+    print("end plot")
 
 
 
@@ -131,18 +156,47 @@ def getENData():
         content = f.read().replace('\n', '').lower()
     return content.split(' ')
 
+def getIMDBData():
+    if os.path.exists('imdb.corpus'):
+        with open('imdb.corpus', 'rb') as f:
+            data = pickle.load(f)
+            return data
+
+    def process(string):
+        string = string.replace('\n', '')
+        string = string.replace('<br />', ' ')
+        for c in '()"\'<>,.':
+            string = string.replace(c, ' '+c+' ')
+        return string
+
+    FP = './aclImdb/train/unsup/'
+    fps = os.listdir(FP)
+    random.shuffle(fps)
+    lines = []
+    for fp in fps:
+        with codecs.open(os.path.join(FP, fp), encoding='utf8') as f:
+            content = f.read()
+        content = process(content)
+        lines.append([item.strip().lower() for item in content.split(' ') if item.strip()])
+    with open('imdb.corpus', 'wb+') as f:
+        pickle.dump(lines, f)
+    return lines
 
 def getVocab(corpus):
     vocab = {}
-    for item in corpus:
-        if item not in vocab:
-            vocab[item] = 0
-        vocab[item] += 1
+    for line in corpus:
+        for item in line:
+            if item not in vocab:
+                vocab[item] = 0
+            vocab[item] += 1
     array = list(vocab.items())
     array.sort(key=lambda x: -x[1])
+    with open('rawvocab.pickle', 'wb+') as f:
+        pickle.dump(array, f)
     return array
 
 def sliceVocab(vocab, length, unk):
+    print(len(vocab))
     minus = length - 1
     usable = vocab[: minus]
     dump = vocab[minus: ]
@@ -170,28 +224,70 @@ def getPair(corpus, window, batch_size):
                 context = []
     yield target, context
 
+def _getPair(corpus, window):
+    """
+    corpus Array<string>
+    """
+    L = len(corpus)
+    target = []
+    context = []
+    ids = [_ for _ in range(L)]
+    # random.shuffle(ids)
+    for process, index in enumerate(ids):
+        word = corpus[index]
+        start = max(0, index - window)
+        end = min(L, index + window + 1)
+        for _context in corpus[start: index] + corpus[index + 1: end]:
+            yield word, _context
+
+def getLinePair(corpus, window, batch_size):
+    """
+    corpus Array<Array<string>>
+    """
+    data = []
+    for line in corpus:
+        data = list(_getPair(line, window))
+        random.shuffle(data)
+        for index in range(0, len(data), batch_size):
+            batch = data[index: index + batch_size]
+            yield [item[0] for item in batch], [item[1] for item in batch]
+
+
 if __name__ == '__main__':
     unk = '<UNK>'
-    batch_size = 20
-    neg_size = 5
+    batch_size = 600
+    neg_size = 60
     embed_dim = 300
 
-    corpus = getCNData()
-    print(len(corpus))
-    vocab = getVocab(corpus)
-    vocab = sliceVocab(vocab, 5000, unk)
-    nce = NCE(vocab, embed_dim, unk)
+    corpus = getIMDBData()
+    print('corpus length', len(corpus))
+    # vocab = getVocab(corpus)
+    with open('rawvocab.pickle', 'rb') as f:
+        vocab = pickle.load(f)
+        print('vocob length', len(vocab))
+    vocab = sliceVocab(vocab, 100000, unk)
+    # nce = NCE(vocab, embed_dim, unk)
+    del vocab
+    nce = torch.load('nce.torch')
 
     # nce.load()
-    # nce.plot(700)
+    # plot(nce, 900)
+    # nce.dump()
     # 1/0
-    optimizer = torch.optim.SGD(nce.parameters(), 0.5)
-    for _ in range(1):
+    optimizer = torch.optim.SGD(nce.parameters(), 0.08)
+    for _ in range(5):
         process = 0
-        for targetwords, contextwords in getPair(corpus, 2, batch_size):
+        sum_loss = 0
+        start = time.time()
+        for targetwords, contextwords in getLinePair(corpus, 2, batch_size):
             targets, contexts = nce.get_targets_n_contexts(targetwords, contextwords)
-            loss = nce.forward(targets, contexts, neg_size, 50)
-            if not process % 5000:
+            loss = nce.forward(targets, contexts, neg_size, 3.0)
+            sum_loss += loss.item()
+            if not process % 1000:
+                print(nce.embed)
+                print(sum_loss / 1000.0)
+                sum_loss = 0
+                start = time.time()
                 nce.store()
                 print("process saved")
             if torch.isinf(loss):
@@ -200,6 +296,14 @@ if __name__ == '__main__':
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
+
+            # clean up
+            del targets
+            del contexts
+            del loss
+
             process += 1
+            if not process % 50:
+                print("remain %0.2f"%(((time.time() - start) / process) * (1000 - process % 1000)))
     nce.store()
     nce.plot()
